ae6449656da19378b890e4d6ab90b3383e73594e
Tushar Rajput <tusharo17rajput@gmail.com>
2024-01-01 23:53:27 +0530

    create workspace form,

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -19,7 +19,7 @@ import Link from "next/link"
 import Image from "next/image"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
-import Loader from "@/components/Loader"
+import Loader from "@/components/global/Loader"
 import { actionLoginUser } from "@/lib/serverActions/auth-actions"
 
 const LoginPage = () => {
--- src/components/dashboard-setup/dashboard-satup.tsx
+++ src/components/dashboard-setup/dashboard-satup.tsx
@@ -1,5 +1,6 @@
+"use client"
 import { AuthUser } from "@supabase/supabase-js"
-import React from "react"
+import React, { useState } from "react"
 import {
   Card,
   CardContent,
@@ -7,16 +8,40 @@ import {
   CardHeader,
   CardTitle,
 } from "../ui/card"
+import EmojiPicker from "../global/EmojiPicker"
+import { Label } from "../ui/label"
+import { Input } from "../ui/input"
+import { FieldValues, useForm } from "react-hook-form"
+import { z } from "zod"
+import { Subscription } from "@/lib/supabase/supabase.types"
+import { Button } from "../ui/button"
+import Loader from "../global/Loader"
+import { CreateWorkspaceFormSchema } from "@/lib/types"
 
 interface DashboardSetupProps {
   user: AuthUser
-  subscription: {} | null
+  subscription: Subscription | null
 }
 
 const DashboardSetup: React.FC<DashboardSetupProps> = ({
   user,
   subscription,
 }) => {
+  const [selectedEmoji, setSelectedEmoji] = useState("🔒")
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { isSubmitting: isLoading, errors },
+  } = useForm<z.infer<typeof CreateWorkspaceFormSchema>>({
+    mode: "onChange",
+    defaultValues: {
+      logo: "",
+      workspaceName: "",
+    },
+  })
+
   return (
     <Card
       className="w-[800px]
@@ -25,7 +50,7 @@ const DashboardSetup: React.FC<DashboardSetupProps> = ({
   "
     >
       <CardHeader>
-        <CardTitle>Create A Workspace</CardTitle>
+        <CardTitle>Create Workspace</CardTitle>
         <CardDescription>
           Lets create a private workspace to get you started.You can add
           collaborators later from the workspace settings tab.
@@ -33,7 +58,79 @@ const DashboardSetup: React.FC<DashboardSetupProps> = ({
       </CardHeader>
       <CardContent>
         <form>
-          <div className="flex flex-col gap-4"></div>
+          <div className="flex flex-col gap-4">
+            <div className="flex items-center gap-4">
+              <div className="text-5xl">
+                <EmojiPicker
+                  getValue={(emoji) => {
+                    setSelectedEmoji(emoji)
+                  }}
+                >
+                  {selectedEmoji}
+                </EmojiPicker>
+              </div>
+              <div className="w-full ">
+                <Label
+                  htmlFor="workspaceName"
+                  className="text-sm
+                  text-muted-foreground
+                "
+                >
+                  Name
+                </Label>
+                <Input
+                  id="workspaceName"
+                  type="text"
+                  placeholder="Workspace Name"
+                  disabled={isLoading}
+                  {...register("workspaceName", {
+                    required: "Workspace name is required",
+                  })}
+                />
+                <small className="text-red-600">
+                  {errors?.workspaceName?.message?.toString()}
+                </small>
+              </div>
+            </div>
+            <div>
+              <Label
+                htmlFor="logo"
+                className="text-sm
+                  text-muted-foreground
+                "
+              >
+                Workspace Logo
+              </Label>
+              <Input
+                id="logo"
+                type="file"
+                accept="image/*"
+                placeholder="Workspace Name"
+                // disabled={isLoading || subscription?.status !== 'active'}
+                {...register("logo", {
+                  required: false,
+                })}
+              />
+              <small className="text-red-600">
+                {errors?.logo?.message?.toString()}
+              </small>
+              {subscription?.status !== "active" && (
+                <small
+                  className="
+                  text-muted-foreground
+                  block
+              "
+                >
+                  To customize your workspace, you need to be on a Pro Plan
+                </small>
+              )}
+            </div>
+            <div className="self-end">
+              <Button disabled={isLoading} type="submit">
+                {!isLoading ? "Create Workspace" : <Loader />}
+              </Button>
+            </div>
+          </div>
         </form>
       </CardContent>
     </Card>
